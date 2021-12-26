8445491c3c5fc8349b24ad1ee0c41a88b59848d0
Vahtee Boo <ronnie.kaa@bigmir.net>
2021-12-26 11:01:21 +0200

    yes

--- Gradients/ContentView.swift
+++ Gradients/ContentView.swift
@@ -9,8 +9,40 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            VStack(spacing: 0) {
+                ZStack {
+                    Color.blue
+                    LinearGradient(colors: [.red, .green, .purple], startPoint: .topLeading, endPoint: .bottomTrailing)
+                        .frame(width: 300, height: 300)
+                    Color.init(red: 0.23, green: 0.45, blue: 0.24, opacity: 0.7)
+                        .frame(minWidth: 40, idealWidth: 100, maxWidth: 200, minHeight: 50, idealHeight: .infinity, maxHeight: 250)
+                    Text("Софія учениця")
+                        .font(.title).bold()
+                    Spacer()
+                }
+                
+                ZStack {
+                    Color.primary
+                    
+                    Color.yellow
+                        .frame(width: 300, height: 300)
+                    Color.blue
+                        .frame(width: 200, height: 200)
+                    Color.secondary
+                        .frame(width: 100, height: 100)
+                    Image(systemName: "sun.min")
+                        .resizable()
+                        .frame(width: 75, height: 75)
+                }
+            }
+            Text("Анна моя доця")
+                .font(.title).bold()
+                .foregroundStyle(.secondary)
+                .padding()
+                .background(.ultraThinMaterial)
+        }
+        .ignoresSafeArea()
     }
 }
 

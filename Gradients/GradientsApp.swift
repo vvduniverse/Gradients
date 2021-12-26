e2ddb691c8dd7fb31f693c9ca7eeea3ae2d9f575
Vahtee Boo <ronnie.kaa@bigmir.net>
2021-12-26 11:00:19 +0200

    Initial Commit

---
+++ Gradients/GradientsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GradientsApp.swift
+//  Gradients
+//
+//  Created by Vahtee Boo on 26.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct GradientsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
